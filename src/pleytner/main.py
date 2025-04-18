b50a47e49064d5ad5d897212addb37fa17c280fc
vagrid <vagrid@zaclys.net>
2025-04-18 10:11:45 +0200

    ✨ feat: add pleytner package structure

---
+++ src/pleytner/main.py
@@ -0,0 +1,7 @@
+"""TODO"""
+
+def return_1_or_2(x: int) -> int:
+    """TODO"""
+    if x > 0:
+        return 2
+    return 1
---
+++ tests/test_main.py
@@ -0,0 +1,9 @@
+"""TODO"""
+
+from pleytner.main import return_1_or_2
+
+def test_return_1_or_2() -> None:
+    """TODO"""
+    assert return_1_or_2(-1) == 1
+    assert return_1_or_2(1) == 2
+
